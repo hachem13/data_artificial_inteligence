faa5ab7b9bb97651d23e8230903df209448c4b66
«hachem» <hachem.mosbah.data@gmail.com>
2020-11-25 17:10:40 +0100

    logistic regression

--- brief_projet/regression_logistique/logistic.py
+++ brief_projet/regression_logistique/logistic.py
@@ -1,10 +1,12 @@
+from sklearn.metrics import precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 import scipy.optimize as opt
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
+
 
 query = pd.read_csv("/home/hachem/Documents/git/data_artificial_inteligence/brief_projet/regression_logistique/data.csv")
+query['Gender'] = pd.get_dummies(query['Gender'])
 
 #x = np.matrix([np.ones(query.shape[0]),query["Age"].values,query["EstimatedSalary"].values])
 x = np.matrix([np.ones(query.shape[0]),query["Gender"].values,query["Age"].values,query["EstimatedSalary"].values])
@@ -30,7 +32,6 @@ class model_regression_logistic():
         #j_theta = -1/m(-y.T.dot(np.log(h)) - (1-y).T.dot(np.log(1 -h)))
         #grad = 1/m (h - y).dot(x)
         j_theta = -1/m*(np.log(self.sigmoid(theta.dot(x))).dot(y.T)+np.log(1-self.sigmoid(theta.dot(x))).dot((1-y).T))
-        print(j_theta)
         grad = (1/m)*(self.sigmoid(theta.dot(x))-y).dot(x.T)
         return [j_theta, grad]
     
